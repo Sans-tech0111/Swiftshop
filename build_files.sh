c0d440f9cf99688bb62cbaf4574584fc80fd231d
Sanskar Jawalkar <78677375+Sans-tech0111@users.noreply.github.com>
2024-01-19 21:47:35 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "START"
-sudo apt-get install libsqlite3-dev
+apt-get install libsqlite3-dev
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "END"
