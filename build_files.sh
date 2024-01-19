7f158826a2b6c4b0404cff5d1468beadbbc5dddd
Sanskar Jawalkar <78677375+Sans-tech0111@users.noreply.github.com>
2024-01-19 21:56:11 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "START"
-python3.10.0 -m pip install -r requirements.txt
-python3.10.0 manage.py collectstatic --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 echo "END"
