d4b24e8bce56979a0431bbe53021e11c499e33e0
Sanskar Jawalkar <78677375+Sans-tech0111@users.noreply.github.com>
2024-01-20 15:59:51 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo "START"
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
 echo "END"
