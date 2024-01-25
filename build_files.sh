f817a07067d3ef658fd30418320710d73a102d7a
Sanskar Jawalkar <78677375+Sans-tech0111@users.noreply.github.com>
2024-01-25 14:08:09 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,17 @@
 echo "START"
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
-python3 manage.py makemigrations
-python3 manage.py migrate
+
+
+PYTHON_PATH=$(which python3.10.7)
+
+
+if [ -z "$PYTHON_PATH" ]; then
+    echo "Python 3.10.7 not found. Make sure it's installed and in your PATH."
+    exit 1
+fi
+
+$PYTHON_PATH -m pip install -r requirements.txt
+$PYTHON_PATH manage.py collectstatic --noinput --clear
+$PYTHON_PATH manage.py makemigrations
+$PYTHON_PATH manage.py migrate
+
 echo "END"
