bfaec760f1424d7beed792a5cefa2791866d7cbe
Sanskar Jawalkar <78677375+Sans-tech0111@users.noreply.github.com>
2024-02-04 23:17:08 +0530

    Update views.py

--- store/views.py
+++ store/views.py
@@ -84,4 +84,28 @@ def processOrder(request):
     return JsonResponse('Payment completed!',safe=False)
 
 def userLogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('password')
+        user = authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('store')
+        else:
+            return HttpResponse("Username or password is incorrect!!!")
+        
     return render(request,'store/login.html')
+
+def userSignUp(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+        if pass1!=pass2:
+            return HttpResponse("Oops! The passwords doesn't match!")
+        else:
+            my_user = User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('user_login')
+    return render(request,'store/signup.html')
